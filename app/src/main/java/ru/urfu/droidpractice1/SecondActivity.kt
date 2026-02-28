26db2f9f1b9beafca5c51a175f571fd8b4a4c33c
sofia.c <sofia.c@email.com>
2026-02-28 19:25:03 +0500

    практика2

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,19 +1,49 @@
 package ru.urfu.droidpractice1
 
-import androidx.activity.ComponentActivity
+import android.content.Intent
 import android.os.Bundle
-import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
+import android.widget.ImageView
+import android.widget.Switch
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.widget.Toolbar
+import coil.load
 
 class SecondActivity : ComponentActivity() {
 
-    private lateinit var binding: ActivitySecondBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySecondBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_second)
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        // Находим View по ID
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val articleImg = findViewById<ImageView>(R.id.articleImage)
+        val switchRead = findViewById<Switch>(R.id.switchRead)
+
+        // Загрузка изображения через Coil
+        articleImg.load("https://www.film.ru/sites/default/files/styles/thumb_1024x450/public/articles/1449341-1137506.jpg")
+
+        // Восстанавливаем состояние чекбокса
+        switchRead.isChecked = savedInstanceState?.getBoolean("isRead") ?: false
+
+        // Обработка кнопки "Назад" на тулбаре
+        toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        //Обработка системной кнопки "Назад"
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val intent = Intent().apply {
+                    putExtra("isRead", switchRead.isChecked)
+                }
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val switchRead = findViewById<Switch>(R.id.switchRead)
+        outState.putBoolean("isRead", switchRead.isChecked)
     }
 }
