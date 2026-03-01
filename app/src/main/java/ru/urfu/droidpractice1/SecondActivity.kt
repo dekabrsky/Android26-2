285699406665f1f6dcc3c3b3fb7db6fb4619d9ab
miracleprime <k.rublev777@gmail.com>
2026-03-01 23:45:09 +0500

    2

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,19 +1,57 @@
 package ru.urfu.droidpractice1
 
-import androidx.activity.ComponentActivity
+import android.content.Context
 import android.os.Bundle
-import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
-class SecondActivity : ComponentActivity() {
+class SecondActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySecondBinding
+    private lateinit var switchRead: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySecondBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_second)
+        Log.d("Lifecycle", "SecondActivity onCreate")
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        switchRead = findViewById(R.id.switchRead)
+
+        val imageView: ImageView = findViewById(R.id.ivArticleImage)
+        imageView.setImageResource(android.R.drawable.ic_menu_gallery)
+        val prefs = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val isRead = prefs.getBoolean("second_article_read", false)
+        switchRead.isChecked = isRead
+
+        switchRead.setOnCheckedChangeListener { _, isChecked ->
+            prefs.edit().putBoolean("second_article_read", isChecked).apply()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "SecondActivity onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "SecondActivity onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "SecondActivity onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "SecondActivity onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "SecondActivity onDestroy")
     }
 }
