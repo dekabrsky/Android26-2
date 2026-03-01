bdfbc0974526402164482fe86b0a1164dc386cfa
Masha <161338993+kormaree@users.noreply.github.com>
2026-03-01 19:46:03 +0500

    done

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -3,13 +3,24 @@ package ru.urfu.droidpractice1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.mutableStateOf
 import ru.urfu.droidpractice1.content.MainActivityScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val isSecondArticleRead = mutableStateOf(false)
+        val getIsArticleHaveRead =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val result = result.data?.getBooleanExtra("isRead", false) ?: false
+                    isSecondArticleRead.value = result
+                }
+            }
         setContent {
-            MainActivityScreen()
+            MainActivityScreen(isSecondArticleRead.value) { intent -> getIsArticleHaveRead.launch(intent)
+            }
         }
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,19 +1,38 @@
 package ru.urfu.droidpractice1
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.activity.ComponentActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Switch
+import androidx.activity.addCallback
+import coil.load
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
 
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val img = findViewById<ImageView>(R.id.articleImage)
+        img.load(getString(R.string.article2_img))
+        val haveRead = findViewById<Switch>(R.id.switchRead)
+        haveRead.isChecked = intent.getBooleanExtra("isRead", false)
 
         binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        onBackPressedDispatcher.addCallback(this) {
+            val intent = Intent().apply {
+                putExtra("isRead", haveRead.isChecked)
+            }
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
