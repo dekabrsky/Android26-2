c89b3898e8a7e622075856288b45403349d4494a
Abdulaziz2180 <azizmakhamov4458@gmail.com>
2026-03-18 15:22:17 +0500

    Исправил SecondActivity на ViewBinding вместо findViewById

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -3,28 +3,29 @@ package ru.urfu.droidpractice1
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
-import android.widget.Switch
 import androidx.activity.ComponentActivity
 import com.bumptech.glide.Glide
+import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
 
 class SecondActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("Lifecycle", "SecondActivity onCreate")
-        setContentView(R.layout.activity_second)
 
-        val imageView = findViewById<ImageView>(R.id.secondImage)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Glide.with(this)
             .load("https://avatars.mds.yandex.net/i?id=bfbbe8812b6eea1ead7123e2a80c04d57b9c7c48-9666026-images-thumbs&n=13")
-            .into(imageView)
+            .into(binding.secondImage)
 
-        val readSwitch = findViewById<Switch>(R.id.readSwitch)
         val wasRead = intent.getBooleanExtra("was_read", false)
-        readSwitch.isChecked = wasRead
+        binding.readSwitch.isChecked = wasRead
 
-        readSwitch.setOnCheckedChangeListener { _, isChecked ->
+        binding.readSwitch.setOnCheckedChangeListener { _, isChecked ->
             val resultIntent = Intent()
             resultIntent.putExtra("is_read", isChecked)
             setResult(RESULT_OK, resultIntent)
